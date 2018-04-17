b7cd8569ef2373a4851c9e2c4aab2fb5049107c7
Robert Laws <roblaws@me.com>
2018-04-17 04:45:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,15 @@ function getFirstSelector(selector) {
 function nestedTarget() {
   return document.querySelector('div#nested div.target');
 }
+
+function increaseByRank(n) {
+  var nodes = document.querySelectorAll('.ranked-list li');
+  for (var i = 0; i < nodes.length; i++) {
+    nodes[i].innerHTML = parseInt(nodes[i].innerHTML + n);
+    
+  }
+}
+
+function deepestChild() {
+  
+}
