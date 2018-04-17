34f9e91fd19976fdd45111032964be807cd06ca8
Robert Laws <roblaws@me.com>
2018-04-17 04:38:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ function getFirstSelector(selector) {
 }
 
 function nextedTarget() {
-  return document.querySelector('div#nested div.target');
+  return document.querySelector('div#nested div.target')[0];
 }
