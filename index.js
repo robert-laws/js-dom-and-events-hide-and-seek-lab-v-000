67f0381a2c479ae5b8346313224eb8c1c04b1de8
Robert Laws <roblaws@me.com>
2018-04-17 05:06:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,5 +14,13 @@ function increaseRankBy(n) {
 }
 
 function deepestChild() {
+  let node = document.getElementById('grand-node')
+  let nextNode = node.children[0]
 
+  while (nextNode) {
+    node = nextNode
+    nextNode = node.children[0]
+  }
+
+  return node
 }
