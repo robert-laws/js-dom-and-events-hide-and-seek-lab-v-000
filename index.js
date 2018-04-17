1953bf5240e16a7fa4987cd13dd89027c057aac1
Robert Laws <roblaws@me.com>
2018-04-17 05:06:12 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,26 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  return document.querySelector('div#nested div.target');
+}
+
+function increaseRankBy(n) {
+  var nodes = document.querySelectorAll('.ranked-list li');
+  for(var i = 0; i < nodes.length; i++) {
+    nodes[i].innerHTML = parseInt(nodes[i].innerHTML) + n;
+  }
+}
+
+function deepestChild() {
+  let node = document.getElementById('grand-node')
+  let nextNode = node.children[0]
+
+  while (nextNode) {
+    node = nextNode
+    nextNode = node.children[0]
+  }
+
+  return node
+}
